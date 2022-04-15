5e754a0c348d9c0941c4590cf0b3e4e27e4cf5be
Your Name <vastas2002@gmail.com>
2022-04-15 10:53:04 +0530

    Image and text to be added

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   async function submit() {
     if (twitter && walletID) {
       setLoading(true);
-      fetch("https://meta-build.vercel.app/api/save", {
+      fetch("http://localhost:3000/api/save", {
         method: "POST",
         headers: {
           Accept: "application/json",
@@ -95,15 +95,21 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>META BUILD</title>
       </Head>
-      {/* <div className="images">
+      <div className="images">
           <Image
             className="img1"
-            src="/Metabuild.png"
+            src="/logo.png"
             alt="METABUILD LOGO"
-            width={100}
-            height={100}
+            // width={200}
+            // height={100}
           />
-          </div> */}
+          </div>
+          <div className="intro">
+            <h4>
+              Welcome to Meta Build! 
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+            </h4>
+          </div>
       <h3 className="twitter-title">Twitter ID</h3>
       <div className="twitter-input">
         <input
@@ -135,7 +141,7 @@ export default function Home() {
       </button>
       <div></div><br />
       <button className="button-style" onClick={submit}>
-        {!isLoading ? "Continue" : "Please Wait"}
+        {!isLoading ? "Submit" : "Please Wait"}
       </button><br />
       <h3 id="error-throwback"></h3>
     </div>
